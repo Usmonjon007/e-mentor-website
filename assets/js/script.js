f4f225ee93ed4a3b208f252386cdb099e9ed631f
Usmonjon Hasanov <usmonjonhasanov777@gmail.com>
2023-08-29 14:14:43 +0500

    Add nav menu list on clicking menu btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+const menuBtnIcon = menuBtn.querySelector("i");
+const menuItems = document.querySelectorAll(".menu-item");
+
+function menuBtnToggler() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        menuBtnIcon.classList.add("fa-bars");
+        menuBtnIcon.classList.remove("fa-x");
+    } else {
+        menu.classList.add("active");
+        menuBtnIcon.classList.remove("fa-bars");
+        menuBtnIcon.classList.add("fa-x");
+    }
+}
+
+menuBtn.addEventListener("click", menuBtnToggler);
+menuItems.forEach((item) => {
+    item.addEventListener("click", menuBtnToggler);
+});
